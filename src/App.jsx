30ff5014a75480d2835a710d266adc07839b360a
GokilaSundar <gokithilaga267@gmail.com>
2024-11-28 15:32:31 +0530

    Added basename

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,7 @@ import Navbar from "./component/Navbar";
 import CurrentDetails from "./component/CurrentDetails";
 import team from "../src/assets/ri_team-fill (1).svg";
 import group from "../src/assets/Group.svg";
-import {
-  HashRouter,
-  Routes,
-  Route,
-  N,
-  HashRouteravigate,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "../src/pages/Dashboard";
 import Feeds from "../src/pages/Feeds";
 import MyDetails from "../src/pages/MyDetails";
@@ -18,25 +12,19 @@ import MyTeamDetails from "../src/pages/MyTeamDetails";
 import OrganisationDetails from "../src/pages/OrganisationDetails";
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename="/demo-keka">
       <div className="m-10">
         <Navbar />
       </div>
       <Routes>
-        <Route
-          path="/demo-keka/"
-          element={<Navigate to="/demo-keka/dashboard" replace />}
-        />
-        <Route path="/demo-keka/dashboard" element={<Dashboard />} />
-        <Route path="/demo-keka/feeds" element={<Feeds />} />
-        <Route path="/demo-keka/my-details" element={<MyDetails />} />
-        <Route path="/demo-keka/my-team-details" element={<MyTeamDetails />} />
-        <Route
-          path="/demo-keka/organisation-details"
-          element={<OrganisationDetails />}
-        />
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/feeds" element={<Feeds />} />
+        <Route path="/my-details" element={<MyDetails />} />
+        <Route path="/my-team-details" element={<MyTeamDetails />} />
+        <Route path="/organisation-details" element={<OrganisationDetails />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
